b6ff7834dae7fc42724d0e45bc30f07eb515ca74
anastassow <127148449+anastassow@users.noreply.github.com>
2026-04-25 16:40:22 +0200

    i am stupid and forgot

--- Formula/sandokan.rb
+++ Formula/sandokan.rb
@@ -1,7 +1,7 @@
 class Sandokan < Formula
   desc "High-performance neural network and deep learning library for C++"
   homepage "https://github.com/Molikano/Sandokan"
-  url "https://github.com/Molikano/Sandokan/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/Molikano/Sandokan/archive/refs/tags/v0.1.3.tar.gz"
   sha256 "6d57112791050c0ac5d1237ac5bfc67a36652b943b470d99d05e407bc5378a8b"
   license "MIT"
   head "https://github.com/Molikano/Sandokan.git", branch: "main"
